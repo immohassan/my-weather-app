8ed1aa1bbf28a59d0189154e8bbede9f0f973d1e
immohassan <immohassan06@gmail.com>
2023-03-17 22:35:23 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Navbar from "./components/Navbar";
+import DisplayTemp from "./components/DisplayTemp";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <DisplayTemp />
+    </div>
+  );
+}
+
+export default App;
