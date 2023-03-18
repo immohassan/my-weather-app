25376fb645f0bad7a49a2268c1365613a837cc24
immohassan <immohassan06@gmail.com>
2023-03-18 12:26:20 +0500

    UI changings

--- src/components/DisplayTemp.js
+++ src/components/DisplayTemp.js
@@ -3,12 +3,13 @@ import { useState } from "react";
 export default function DisplayTemp() {
 
     const [data,setdata] = useState({
-        temp: '',
-        feels_like : '',
+        temp: '-',
+        feels_like : '-',
         wind: '-',
         humidity :'-',
         name:'',
-        desc : ''
+        desc : '',
+        code: ''
     });
     const [location,setlocation] = useState(null);
 
@@ -21,37 +22,41 @@ export default function DisplayTemp() {
         const response = await fetch(url);
         const result = await response.json();
         setdata({
-            temp: result.main.temp,
-        feels_like : result.main.feels_like,
+            temp: Math.round(result.main.temp),
+        feels_like : Math.round(result.main.feels_like),
         wind: result.wind.speed,
         humidity:result.main.humidity,
         name:result.name,
-        desc : result.weather[0].main
+        desc : result.weather[0].main,
+        code: result.cod
         });
         console.log(result);
+    
     }
+
 
 
     return (  <div className="container pt-3" > 
             <div className="col-sm-4 m offset-md-4 mt-5 text-center">               
-            <input class="form-control bg-body-secondary p-3" type="text" onChange={handleCity} placeholder="Search City" aria-label="default input example"/>
-            <button type="button" class="btn btn-dark mt-3 px-4 py-2" onClick={handlesubmission}>Search</button>
+            <input className="form-control bg-body-secondary p-3" type="text" onChange={handleCity} placeholder="Search City" aria-label="default input example"/>
+            <button type="button" className="btn btn-dark mt-3 px-4 py-2" onClick={handlesubmission}>Search</button>
             <div className="container-md mt-4 p-4 bg-dark rounded">
             <h1 className="text-white fw-bold ">{data.name}</h1>
-            <h2 className="text-white ">{Math.round(data.temp)} °C</h2>
+            <h2 className="text-white ">{data.temp} °C</h2>
             <p className="text-white fw-semibold">{data.desc}</p>
-            <div class="row align-items-center gap-3 pt-4">
-                                <div class="col text-white fw-bold">
+            <div className="row align-items-center gap-3 pt-4">
+                
+                                <div className="col text-white fw-bold">
                                 Feels like
                                 <br></br>
-                                <p className="fw-light">{Math.round(data.feels_like)} °C </p>
+                                <p className="fw-light">{data.feels_like} °C </p>
                                 </div>
-                                <div class="col text-white fw-bold">
+                                <div className="col text-white fw-bold">
                                 Humidity
                                 <br></br>
                                 <p className="fw-light">{data.humidity} % </p>
                                 </div>
-                                <div class="col text-white fw-bold">
+                                <div className="col text-white fw-bold">
                                 Wind
                                 <br></br>
                                 <p className="fw-light">{data.wind} m/s</p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 export default function Navbar() {
   return (
     <div>
-            <nav class="navbar bg-dark" data-bs-theme="dark">
-        <div class="container-fluid">
-            <span class="navbar-brand mb-0 h1">Weather</span>
+            <nav className="navbar bg-dark" data-bs-theme="dark">
+        <div className="container-fluid">
+            <span className="navbar-brand mb-0 h1">Weather</span>
         </div>
         </nav>
     </div>
